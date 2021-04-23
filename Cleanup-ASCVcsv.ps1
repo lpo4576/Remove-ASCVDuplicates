ed9bfbe8a5704cd5460b45925226a298a70118b6
Larkin <55812754+lpo4576@users.noreply.github.com>
2021-04-23 12:52:31 -0500

    Uploaded initial version

---
+++ Cleanup-ASCVcsv.ps1
@@ -0,0 +1,15 @@
+﻿# C:\Users\312127\Desktop\New_folder
+$Logfile = "\\ctsfile\public\larkin1\ASCVScript.log"
+Function LogWrite
+{
+   Param ([string]$logstring)
+
+   Add-content $Logfile -value $logstring
+}
+$items = get-childitem -Path \\ctsfile\public\stlouis\ASCV_Pull_list\*.csv -Recurse | Where-Object {$_.LastWriteTime -lt (get-date).AddDays(-7)}
+$items | Remove-Item
+$rmitems = $items | select -Property Name
+#[string]::Join("; ",$rmitems.name)
+logwrite -logstring "----------------------------------------"
+logwrite -logstring "Cleanup script removed: $([string]::Join("; ",$rmitems.name))"
+
